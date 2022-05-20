c2ef491ee5aa4007f59c2540b51f6101148af3ca
my-govnocode <64898477+my-govnocode@users.noreply.github.com>
2022-05-20 13:39:55 +0300

    Update SeedController.php

--- www/katalog/commands/SeedController.php
+++ www/katalog/commands/SeedController.php
@@ -4,13 +4,18 @@ namespace app\commands;
 
 use app\models\GroupProperty;
 use app\models\Property;
+use app\models\Product;
 use yii\helpers\Console;
 use yii\console\ExitCode;
+use Faker\Factory;
+use Yii;
 
 class SeedController extends \yii\console\Controller
 {
     public function actionIndex()
     {
+        $faker = Factory::create();
+        
         $groupRazmer = new GroupProperty();
         $groupRazmer->name = 'Размер';
         $groupRazmer->code = 'razmer';
@@ -41,8 +46,43 @@ class SeedController extends \yii\console\Controller
             $propertyGood->group_id = $groupTkan->id;
 
             if ($propertyBad->save() &&  $propertyGood->save() && $propertyBig->save() && $propertySmall->save()) {
-                echo $this->ansiFormat('Заполнение прошло успешно', Console::FG_GREEN);
-                return ExitCode::OK;
+                $products = [];
+
+                for ($i=0;$i<10;$i++) {
+                    $products[] = [
+                        $faker->name,
+                        $faker->randomFloat(2, Product::PRICE_MIN, Product::PRICE_MAX)
+                    ];
+                }
+
+                $productsCreate = Yii::$app->db->createCommand()->batchInsert('products', ['name', 'price'], $products)->execute();
+
+                if ($productsCreate != 0) {
+
+                $productsFirst10 = Product::find()->orderBy(['id' => SORT_ASC])->limit(10)->all();
+                $properties = [$propertyBig, $propertySmall, $propertyBad, $propertyGood];
+                $arr = [];
+
+                foreach ($productsFirst10 as $product) {
+                    $property = $properties[rand(0, count($properties)-1)];
+                    $arr[] = [
+                        $property->id,
+                        $product->id,
+                        $property->group_id
+                    ];
+                }
+
+                $PropertyProduct = Yii::$app->db->createCommand()->batchInsert('property_product', ['property_id', 'product_id', 'group_id'], $arr)->execute();
+                    if ($PropertyProduct != 0) {
+                        echo $this->ansiFormat('Заполнение прошло успешно', Console::FG_GREEN);
+                        return ExitCode::OK;
+                    } else {
+                        echo $this->ansiFormat('Связи не создались', Console::FG_YELLOW);
+                    }
+                } else {
+                    echo $this->ansiFormat('Продукты не создались', Console::FG_YELLOW);
+                }
+
             } else {
                 echo $this->ansiFormat('Свойства не создались', Console::FG_YELLOW);
             }
